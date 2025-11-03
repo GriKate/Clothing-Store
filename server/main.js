19e17c110e1c473d1114e212ecb72d9f79382434
Kate Grigoryeva <kate23april@yandex.ru>
2025-11-03 21:10:53 +0300

    modify main.js

--- server/main.js
+++ server/main.js
@@ -14,7 +14,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// app.use('/', express.static('public'));
+app.use('/', express.static('public'));
 
 // app.use('/catalog', express.static('public/catalog.html'));
 app.use('/goods', catalog);
